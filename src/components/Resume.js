41c7087a8f60e4b70686d810c3180caa5e6318cc
jasmith188 <jasmith.188@gmail.com>
2021-11-15 09:30:34 -0500

    add bootstrap carosel to resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,19 +2,28 @@ import React from 'react';
 import './Resume.css';
 import page1 from '../images/resume-page-1.jpg';
 import page2 from '../images/resume-page-2.jpg';
+import { Carousel } from 'react-bootstrap';
 
 const Resume = () => {
   return (
     <div className="resume">
-      <h1>Resume</h1>
-      <div className="resume-screenshot">
-        <a href={page1}>
-          <img src={page1} alt="resume-screenshot" />
-        </a>
-        <a href={page2}>
-          <img src={page2} alt="resume-screenshot" />
-        </a>
-      </div>
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={page1} alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={page2} alt="Second slide" />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
